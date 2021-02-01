38d5464b234baba2094298d9341d3927f346a828
Anish <sunaranish17@gmail.com>
2021-02-01 14:17:46 +0545

    Question 3

--- Q3/controllers/article.js
+++ Q3/controllers/article.js
@@ -9,5 +9,44 @@ exports.getComments = (req, res) => {
                 const comments = articles.comments;
                 return res.status(200).json({ comments });
             }
-        })
-}; 
+        });
+};
+
+exports.getArticle = (req, res) => {
+    Article.findOne({ _id: req.params.articleId })
+        .populate("postedBy", "_id")
+        .exec((error, article) => {
+            if (error) return res.status(400).json({ error });
+
+            if (article) {
+                return res.status(200).json({ article });
+            }
+        });
+}
+
+exports.getArticleWithComments = (req, res) => {
+    Article.find({})
+        .exec((error, articles) => {
+            if (error) return res.status(400).json({ error });
+
+            if (articles) {
+                const comments = articles.comments;
+                if (comments.length >= 1) {
+                    return res.status(200).json({ articles });
+                } else {
+                    return res.status(204).json({ message: "No content" });
+                }
+            }
+        });
+}
+
+exports.getArticlesOfAuthor = (req, res) => {
+    Article.find({ _id: req.body.authorId })
+        .exec((error, result) => {
+            if (error) return res.status(400).json({ error });
+
+            if (result) {
+                return res.status(200).json({ result });
+            }
+        });
+}
